bb16836fd662ee196611f0a12338ff77c91dda8c
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-24 11:51:32 -0500

    cambiando bd a postgresql

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_ecommerce_g20',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'codigo',
+        'PASSWORD': 'codigo2022',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
 # Password validation
